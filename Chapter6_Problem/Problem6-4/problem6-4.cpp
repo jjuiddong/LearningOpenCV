388460a0a518e33750fcb84abcd1cf79bb50df7d
lee jaejung <jjuiddong@gmail.com>
2016-04-10 20:25:54 +0900

    Problem6-4

--- Chapter6_Problem/Problem6-4/problem6-4.cpp
+++ Chapter6_Problem/Problem6-4/problem6-4.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <sstream>
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/video/background_segm.hpp"
@@ -15,87 +16,26 @@ using namespace cv;
 
 void main()
 {
-	Mat src = imread("../../media/lena.jpg");
+	Mat src = imread("../../media/target.png");
 	cvtColor(src, src, CV_RGB2GRAY);
+	threshold(src, src, 10, 255, CV_THRESH_BINARY_INV);
 	imshow("src", src);
 
+	int arr[4] = { 3,5,9,13 };
+	for (int i=0; i < 4; ++i)
 	{
-		float ar1[] = {
-			-3, 0, 3,
-			-10, 0, 10,
-			-3, 0, 3,
-		};
-
+		Mat dst1;
+		Sobel(src, dst1, CV_16S, 1, 0, arr[i]);
+		Mat dst2;
+		Sobel(src, dst2, CV_16S, 0, 1, arr[i]);
 		Mat dst;
-		Mat kernel1(3, 3, CV_32FC1);
-		for (int i = 0; i < 9; ++i)
-		{
-			kernel1.at<float>(i / 3, i % 3) = ar1[i];
-		}
-
-		cv::filter2D(src, dst, CV_32F, kernel1);
+		addWeighted(dst1, 0.5, dst2, 0.5, 0, dst);
 		convertScaleAbs(dst, dst);
-		imshow("dst1", dst);
 
-		float ar2[] = { -3, 0, 3 };
-		Mat kernel2(1, 3, CV_32FC1);
-		for (int i = 0; i < 3; ++i)
-		{
-			kernel2.at<float>(i / 3, i % 3) = ar2[i];
-		}
-
-		float ar3[] = { 1, 10.f / 3.f, 1 };
-		Mat kernel3(3, 1, CV_32FC1);
-		for (int i = 0; i < 3; ++i)
-		{
-			kernel3.at<float>(i % 3, i / 3) = ar3[i];
-		}
-
-		cv::filter2D(src, dst, CV_32F, kernel2);
-		cv::filter2D(dst, dst, CV_32F, kernel3);
-		convertScaleAbs(dst, dst);
-		imshow("dst 1x3 - 3x1", dst);
+		stringstream ss;
+		ss << "dst " << arr[i];
+		imshow(ss.str().c_str(), dst);
 	}
-
-
-	{
-		float ar1[] = {
-			-3, -10, -3,
-			0, 0, 0,
-			3, 10, 3,
-		};
-
-		Mat dst;
-		Mat kernel1(3, 3, CV_32FC1);
-		for (int i = 0; i < 9; ++i)
-		{
-			kernel1.at<float>(i / 3, i % 3) = ar1[i];
-		}
-
-		cv::filter2D(src, dst, CV_32F, kernel1);
-		convertScaleAbs(dst, dst);
-		imshow("dst2", dst);
-
-		float ar2[] = { 3, 10, 3 };
-		Mat kernel2(1, 3, CV_32FC1);
-		for (int i = 0; i < 3; ++i)
-		{
-			kernel2.at<float>(i / 3, i % 3) = ar2[i];
-		}
-
-		float ar3[] = { -1, 0, 1 };
-		Mat kernel3(3, 1, CV_32FC1);
-		for (int i = 0; i < 3; ++i)
-		{
-			kernel3.at<float>(i % 3, i / 3) = ar3[i];
-		}
-
-		cv::filter2D(src, dst, CV_32F, kernel2);
-		cv::filter2D(dst, dst, CV_32F, kernel3);
-		convertScaleAbs(dst, dst);
-		imshow("dst2 1x3 - 3x1", dst);
-	}
-
 
 	waitKey();
 }
